cebd78908e35013a3bdac38fcf1d2f4239abf26a
Janet Webster <twixmixy@gmail.com>
2023-10-17 22:15:23 -0400

    input validation is working!

--- index.js
+++ index.js
@@ -15,53 +15,133 @@ const questions = [
                 type: 'input',
                 name: 'fullName',
                 message: 'What is your full name?',
+                validate: fullNameInput => {
+                    if (fullNameInput) {
+                        return true;
+                    } else {
+                        console.log('You must enter your name to continue.');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'title',
                 message: 'What is the title of your project?',
+                validate: titleInput => {
+                    if (titleInput) {
+                        return true;
+                    } else {
+                        console.log('You must enter a title to continue.');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'description',
                 message: 'What is a comprehensive description of your project?',
+                validate: descriptionInput => {
+                    if (descriptionInput) {
+                        return true;
+                    } else {
+                        console.log('You must enter a description to continue.');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'installation',
                 message: 'What are the installation instructions for your project?',
+                validate: installationInput => {
+                    if (installationInput) {
+                        return true;
+                    } else {
+                        console.log('You must enter installation instructions to continue.');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'usage',
                 message: 'How is this project meant to be used?',
+                validate: usageInput => {
+                    if (usageInput) {
+                        return true;
+                    } else {
+                        console.log('You must enter usage directions to continue.');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'contribution',
                 message: 'What are the contribution guidelines?',
+                validate: contributionInput => {
+                    if (contributionInput) {
+                        return true;
+                    } else {
+                        console.log('You must enter contribution guidelines to continue.');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'test',
                 message: 'What are the testing procedures?',
+                validate: testInput => {
+                    if (testInput) {
+                        return true;
+                    } else {
+                        console.log('You must enter test procedures to continue.');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'list',
                 name: 'license',
                 message: 'Choose a license',
                 choices: ["GNU", "MIT", "The Unlicense"],
-                default: "MIT"
+                default: "MIT",
+                validate: licenseInput => {
+                    if (licenseInput) {
+                        return true;
+                    } else {
+                        console.log('You must choose a license to continue.');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'github',
                 message: 'What is your github handle?',
+                validate: githubInput => {
+                    if (githubInput) {
+                        return true;
+                    } else {
+                        console.log('You must enter a github handle to continue.');
+                        return false;
+                    }
+                }
             },
             {
                 type: 'input',
                 name: 'email',
                 message: 'What is your email?',
+                validate: emailInput => {
+                    if (emailInput) {
+                        return true;
+                    } else {
+                        console.log('You must enter an email to continue.');
+                        return false;
+                    }
+                }
             }
         ])
         .then((answers) => {
