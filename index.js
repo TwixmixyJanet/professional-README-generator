c4813b7a8728d3fe8dcacca8a1734fe4f2c52c19
Janet Webster <twixmixy@gmail.com>
2023-10-17 20:53:53 -0400

    added require inquirer variable

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ Follow the prompts to create your very own README markdown file!
 `);
 
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
